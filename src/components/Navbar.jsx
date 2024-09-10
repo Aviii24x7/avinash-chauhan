b2c46446368dc1814867289633e40e170fca100b
Avinash Chauhan 202073 <chauhanavi737@gmail.com>
2024-09-11 00:24:22 +0530

    end commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -122,7 +122,7 @@ export default function Navbar() {
 
           </li>
           <li className="rounded w-[150px] h-[50px] flex justify-between items-center px-[11px] ml-[-100px] hover:ml-[-10px] duration-500 bg-[#522c48]">
-            <a className="flex justify-between items-center w-full text-gray-100 font-semibold" href="https://drive.google.com/file/d/1YqRHinT5IlVhhAskl9Qyg8OkYfsrKFSb/view?usp=sharing" target="_blank">
+            <a className="flex justify-between items-center w-full text-gray-100 font-semibold" href="https://drive.google.com/file/d/1mRqoODQycatJknUZDZiu7DYcYamidAEV/view?usp=sharing" target="_blank">
 
               Resume <BsFillPersonLinesFill size={30} />
 
